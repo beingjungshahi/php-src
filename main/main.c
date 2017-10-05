e8225eb2fca6d711189d2ab8f12055fb6c1b188b
Peter Kokot <peterkokot@gmail.com>
2017-10-05 19:07:28 +0200

    Remove old apache_hooks occurrence
    The apache_hooks SAPI has been removed since PHP >= 7.0 and there is no
    need to have two different php_request_startup definitions.

--- main/main.c
+++ main/main.c
@@ -1622,7 +1622,6 @@ static int php_start_sapi(void)
 
 /* {{{ php_request_startup
  */
-#ifndef APACHE_HOOKS
 int php_request_startup(void)
 {
 	int retval = SUCCESS;
@@ -1702,37 +1701,6 @@ int php_request_startup(void)
 
 	return retval;
 }
-# else
-int php_request_startup(void)
-{
-	int retval = SUCCESS;
-
-	zend_interned_strings_activate();
-
-#if PHP_SIGCHILD
-	signal(SIGCHLD, sigchld_handler);
-#endif
-
-	if (php_start_sapi() == FAILURE) {
-		return FAILURE;
-	}
-
-	php_output_activate();
-	sapi_activate();
-	php_hash_environment();
-
-	zend_try {
-		PG(during_request_startup) = 1;
-		if (PG(expose_php)) {
-			sapi_add_header(SAPI_PHP_VERSION_HEADER, sizeof(SAPI_PHP_VERSION_HEADER)-1, 1);
-		}
-	} zend_catch {
-		retval = FAILURE;
-	} zend_end_try();
-
-	return retval;
-}
-# endif
 /* }}} */
 
 /* {{{ php_request_startup_for_hook
